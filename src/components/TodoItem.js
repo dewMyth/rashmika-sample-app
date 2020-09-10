5022e027c93b3ef828fdfa3c9f410a265359a689
dewMyth <akalankadewmith@gmail.com>
2020-09-10 09:11:00 +0530

    Recorrected

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,43 +1,37 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class TodoItem extends Component {
-    getStyle = () => {
-        return {
-            background :'#f4f4f4',
-            padding :'10px',
-            borderBottom :'1px #ccc dotted',
-            textDecoration:this.props.todo.completed ? 'line-through' : 'none'
-        }
-    }
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+  };
 
-    render() {
-        return (
-            <div style={this.getStyle()}>
-                
-                <p>
-                    <input type="checkbox" onChange={this.props.markComplete.bind(this,
-                        this.props.id)} />
-                    {' '} {this.props.todo.title}
-                </p>
-            </div>
-        )
-    }
-
-    }
-
-    
-
-
-//const itemStyle = {backgroundColor :'#f4f4f4'}//mehema constant ekk pass karala
-// inline css dennat pluwan 
-//  <div style={{backgroundColor :'#f4f4f4}}> puluwan.
-
-
-
-TodoItem.propTypes = {
-    todo : PropTypes.object.isRequired
+  render() {
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, this.props.todo.id)}
+          />
+          {this.props.todo.title}
+        </p>
+      </div>
+    );
+  }
 }
 
-export default TodoItem
+//const itemStyle = {backgroundColor :'#f4f4f4'}//mehema constant ekk pass karala
+// inline css dennat pluwan
+//  <div style={{backgroundColor :'#f4f4f4}}> puluwan.
 
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+};
+
+export default TodoItem;
